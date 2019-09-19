b105945f61d1a73f3e1d0d0b0aa4cf1b6b17c986
Elijah Mooring <ejsisgreat@gmail.com>
2019-09-19 07:47:49 -0500

    Create first test

---
+++ tests/unit/index.test.js
@@ -0,0 +1,9 @@
+const sayHello = require('../../');
+
+describe('sayHello', () => {
+	
+	it(`Should return a gretting containing the name`, () => {
+		const res = sayHello('Elijah');
+		expect(res).toMatch(/Elijah/);
+	})
+})
