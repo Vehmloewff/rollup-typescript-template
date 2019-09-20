00e18df85bbc3c1c18b0ccef8466b640488f3e13
Elijah Mooring <ejsisgreat@gmail.com>
2019-09-20 13:54:40 -0500

    Add debug

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const debug = require('./debug')('startup');
+	
 function sayHello(name) {
+	debug.error('Failed!');
+	debug.warn('Warning!');
+	debug.notice('Notice...');
+	debug.info('Up to date!');
+
 	return `Hello, ${name}!`;
 }
 
