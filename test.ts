34d6baace247a180a17de10cbbded3200d45104d
Elijah <ejsisgreat@gmail.com>
2019-11-08 08:13:44 -0600

    Run lint

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import { sayHello } from './src';
 import { it } from 'zip-tap';
 
-it(`sayHello should return a gretting`, expect => {
+it(`sayHello should return a gretting`, (expect) => {
 	expect(sayHello(`Elijah`)).toMatch(/hello.*Elijah/i);
 });
