da0d788575fc0f5a764078c9f712d8c69ffc2581
Elijah Mooring <ejsisgreat@gmail.com>
2019-11-11 11:24:16 -0600

    Add zip-tap

--- test.ts
+++
@@ -1,6 +0,0 @@
-import { sayHello } from './src';
-import { it } from 'zip-tap';
-
-it(`sayHello should return a gretting`, (expect) => {
-	expect(sayHello(`Elijah`)).toMatch(/hello.*Elijah/i);
-});
